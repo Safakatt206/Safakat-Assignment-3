be5e6f62b6e031421fe6269034221e68c8155c83
unknown <you@example.com>
2025-02-01 02:12:05 +0600

    3rd commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
+    theme: { colors: {
+      
+    },
+      extend: {
+        
+      },
     },
     plugins: [],
   }
+  
+  
+   
